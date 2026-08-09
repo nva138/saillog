ddcc49e3cd2accb31a10facf8346ad577d7dcd4e
Maximilian Mayer <office@maximilianmayer.at>
2026-08-09 19:19:23 +0200

    feat: trip named and saved per alert

--- src/types/Trip.ts
+++ src/types/Trip.ts
@@ -1,9 +1,7 @@
-import { storage } from '/utils/storage';
-
 export interface Trip{
     id: number
     name: string
     eventDate: string
     startingTime: string
-    finishingTime: string
+    finishingTime?: string
 }
--- src/views/TripListView.vue
+++ src/views/TripListView.vue
@@ -10,10 +10,50 @@ import {
   IonContent,
   IonList,
   IonItem,
-  IonLabel
+  IonLabel,
+  IonAlert,
+  IonButton
 } from '@ionic/vue';
 
 const tripList = ref<Trip[]>([]);
+const isAlertOpen = ref(false);
+const setAlertOpen = (state: boolean) => {
+  isAlertOpen.value = state;
+}
+
+const alertInput = [
+  {
+    name: "tripName",
+    type: "text" as const,
+    placeholder: "Trip in Italy"
+  }
+];
+
+const alertButton = [
+    {
+      text: "cancel",
+      role:"cancel"
+},
+  {
+    text: "Start",
+    handler: (data: {tripName: string}) => {
+      if(!data.tripName || data.tripName.trim() === "") {
+        return false;
+      }
+      const newTrip =
+          {
+            id: Date.now(),
+            name: data.tripName,
+            eventDate: new Date().toLocaleDateString('de-DE'),
+            startingTime: new Date().toLocaleString('de-DE'),
+          };
+      addTrip(newTrip);
+      tripList.value = getTrips();
+    }
+  }
+];
+
+
 
 onMounted(() => (
     tripList.value = getTrips()
@@ -39,7 +79,17 @@ onMounted(() => (
           </ion-label>
         </ion-item>
       </ion-list>
-      <ion-button fill="outline" @click="">start trip!</ion-button>
+      <ion-button fill="outline" @click="setAlertOpen(true)">start trip!</ion-button>
+
+      <ion-alert
+          :is-open="isAlertOpen"
+          header="Neuen Trip starten"
+          sub-header="Wohin geht die Reise?"
+          :inputs="alertInput"
+          :buttons="alertButton"
+          @didDismiss="setAlertOpen(false)"
+      ></ion-alert>
+
     </ion-content>
   </ion-page>
 </template>
