19419820b83a2ed01b5a36ac8447595ecbf80189
Maximilian Mayer <office@maximilianmayer.at>
2026-08-12 12:28:38 +0200

    feat: detail-view shows entries on the map

--- src/views/TripDetailsView.vue
+++ src/views/TripDetailsView.vue
@@ -7,17 +7,35 @@ import {
   IonContent, IonList, IonItem, IonLabel,
 } from '@ionic/vue';
 import {useRoute} from "vue-router";
-import {getTripById, getLogbookEntriesByTripId} from "@/utils/storage";
-import {onMounted, ref} from "vue";
+import {getTripById, getLogbookEntriesByTripId, loadTrackPointsForTrip} from "@/utils/storage";
+import {onMounted, onUnmounted, ref} from "vue";
 import {LogbookEntry} from "@/types/LogbookEntry";
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 const route = useRoute();
 const trip = getTripById(Number(route.params.id));
 const logbookEntries = ref<LogbookEntry[]>([])
+let map: L.Map
 
-onMounted(() =>
-    logbookEntries.value = getLogbookEntriesByTripId(Number(route.params.id))
+onMounted(() => {
+      map = L.map("detailMap").setView([48.21, 16.37], 13);
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map)
+      setTimeout(() => map.invalidateSize(), 100);
+  logbookEntries.value = getLogbookEntriesByTripId(Number(route.params.id));
+  const points = loadTrackPointsForTrip(Number(route.params.id));
+  logbookEntries.value.forEach(e =>
+      L.marker([e.lat, e.lon]).addTo(map).bindPopup(e.note ?? "")
+  );
+  L.polyline(points.map(p => [p.lat, p.lon])).addTo(map);
+
+
+    }
 );
+
+onUnmounted(() => {
+  map.remove();
+});
 
 </script>
 
@@ -42,6 +60,7 @@ onMounted(() =>
             </ion-label>
           </ion-item>
         </ion-list>
+        <div id="detailMap" style="height: 400px"></div>
         <ion-list>
           <ion-item v-for="entry in logbookEntries" :key="entry.id">
             <ion-label>{{ entry.note }} {{ entry.time }}</ion-label>
